67b8511e109fc4ce1ffe646629c84baafb0ff498
AletheiaC <68302999+AletheiaC@users.noreply.github.com>
2020-08-11 15:44:50 -0500

    Index.html and variables.jss

---
+++ variables.js
@@ -0,0 +1,37 @@
+//1.
+//var a = 'Hello';
+//var b = 'World';
+//function sayingHi(a,b) {
+//   return a + " " + b;
+//}
+//console.log(sayingHi(a,b));
+
+//2.
+//function print() {
+ // var a = 'javascript';  
+ // return a;
+//}
+
+//console.log(print());
+
+
+
+//3.
+//function properGreeting() {
+  //  var hour = new Date().getHours();
+   //if (hour <= 12) {
+     //   return "Good morning";
+   //} else {
+     //   return "Good Evening";
+   //}
+//}
+
+//console.log (properGreeting());
+
+//4.
+//function add(num1, num2) {
+ //   return num1 + num2;
+//}
+
+//console.log(add(2, 2));
+//console.log(add(20, 10));
